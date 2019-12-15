669ce4f2b150c21bfb4ddff5b9a1410d140f80ef
Kedlawv <waldek.bus@gmail.com>
2019-12-15 14:07:37 +0100

    Order domain model | OrderController @ /orders -> orderForm()  Get @ /current

---
+++ src/main/java/tacos/domain/Order.java
@@ -0,0 +1,118 @@
+package tacos.domain;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.hibernate.validator.constraints.NotBlank;
+import java.util.Objects;
+
+public class Order {
+
+    public Order() {
+    }
+
+    public Order(String name, String street, String city, String state, String zip,
+                 String ccNumber, String ccExpiration, String ccCVV) {
+        this.name = name;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.ccNumber = ccNumber;
+        this.ccExpiration = ccExpiration;
+        this.ccCVV = ccCVV;
+    }
+
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCcNumber() {
+        return ccNumber;
+    }
+
+    public void setCcNumber(String ccNumber) {
+        this.ccNumber = ccNumber;
+    }
+
+    public String getCcExpiration() {
+        return ccExpiration;
+    }
+
+    public void setCcExpiration(String ccExpiration) {
+        this.ccExpiration = ccExpiration;
+    }
+
+    public String getCcCVV() {
+        return ccCVV;
+    }
+
+    public void setCcCVV(String ccCVV) {
+        this.ccCVV = ccCVV;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(name, order.name) &&
+                Objects.equals(street, order.street) &&
+                Objects.equals(city, order.city) &&
+                Objects.equals(state, order.state) &&
+                Objects.equals(zip, order.zip) &&
+                Objects.equals(ccNumber, order.ccNumber) &&
+                Objects.equals(ccExpiration, order.ccExpiration) &&
+                Objects.equals(ccCVV, order.ccCVV);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, street, city, state, zip, ccNumber, ccExpiration, ccCVV);
+    }
+}
