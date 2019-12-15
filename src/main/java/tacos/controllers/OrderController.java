9b67340c223b5088faf60a0282fd92f81ffc6353
Kedlawv <waldek.bus@gmail.com>
2019-12-15 14:36:45 +0100

    OrderController -> processOrder() POST @ /orders

--- src/main/java/tacos/controllers/OrderController.java
+++ src/main/java/tacos/controllers/OrderController.java
@@ -1,22 +1,29 @@
 package tacos.controllers;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.domain.Order;
 
-
-@Slf4j
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
+
+    private static final org.slf4j.Logger log =
+            org.slf4j.LoggerFactory.getLogger(OrderController.class);
 
     @GetMapping("/current")
     public String orderForm(Model model){
         model.addAttribute("order", new Order());
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order){
+        log.info("Order submitted: " + order);
+        return "redirect:/";
     }
 
 }
