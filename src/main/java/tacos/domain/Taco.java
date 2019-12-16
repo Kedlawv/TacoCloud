8aecfb77ca49fa42758c116cdd1b6eaa28f55df0
Kedlawv <waldek.bus@gmail.com>
2019-12-16 13:19:36 +0100

    Taco and Order | Validation Annotation

--- src/main/java/tacos/domain/Taco.java
+++ src/main/java/tacos/domain/Taco.java
@@ -1,10 +1,17 @@
 package tacos.domain;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 import java.util.Objects;
 
 public class Taco {
+
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
+
+    @Size(min=1, message="You must choose at least 1 ingredient")
     private List<String> ingredients;
 
     public Taco() {
