9561457321c01edb9170eff22d5bb6b0bff7295e
Kedlawv <waldek.bus@gmail.com>
2019-12-26 18:59:21 +0100

    Removed spring-security dependency | clean up classes | app works as expected

--- src/main/java/tacos/domain/User.java
+++ src/main/java/tacos/domain/User.java
@@ -1,16 +1,10 @@
 package tacos.domain;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.*;
-import java.util.Arrays;
-import java.util.Collection;
 
 @Entity
 @Table(name = "Users")
-public class User implements UserDetails {
+public class User  {
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
@@ -19,9 +13,6 @@ public class User implements UserDetails {
     private  String username;
     private  String password;
     private  String fullname;
-    private boolean enabled;
-
-    Collection<? extends GrantedAuthority> authorities;
 
     public User() {
     }
@@ -30,20 +21,13 @@ public class User implements UserDetails {
         this.username = username;
         this.password = password;
         this.fullname = fullname;
-        this.enabled = true;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
-    }
-
-    @Override
     public String getPassword() {
         return this.password;
     }
 
-    @Override
+
     public String getUsername() {
         return this.username;
     }
@@ -72,23 +56,5 @@ public class User implements UserDetails {
         return fullname;
     }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
